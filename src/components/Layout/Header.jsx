95978fa89b2e1bde26e92d94c330293c5290628a
nuruddinsayeed <nuruddinsayeed@gmail.com>
2021-05-14 22:19:41 +0600

    Header with banner added

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import mealsImg from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+
+function Header(props) {
+    return (
+        <React.Fragment>
+            <header className={classes.header}>
+                <h1>React Meals</h1>
+                <button>Cart</button>
+            </header>
+            <div className={classes['main-image']}>
+                <img src={mealsImg} alt="Table decorated with food!" />
+            </div>
+        </React.Fragment>
+    );
+};
+
+export default Header
